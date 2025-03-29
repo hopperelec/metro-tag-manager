933f5a9c7d24a071d3071f55e2032cfcef214d98
hopperelec <hopperelec@gmail.com>
2025-03-29 01:34:58 +0000

    Only ffprobe new files when scanning

--- src/lib/server/scan.ts
+++ src/lib/server/scan.ts
@@ -10,7 +10,7 @@ import {
 import prisma from "$lib/server/prisma";
 import ffmpeg from "fluent-ffmpeg";
 
-type FileData = {
+type MediaData = {
 	path: string;
 	size: number;
 	width: number | undefined;
@@ -18,45 +18,60 @@ type FileData = {
 	duration: number | undefined;
 };
 
-async function scan(relativeDir = ""): Promise<FileData[]> {
+async function getCurrentFiles(relativeDir = "") {
 	const files = await fs.readdir(path.join(MEDIAS_PATH, relativeDir));
-	const medias: FileData[] = [];
+	const result = [] as {
+		absolutePath: string;
+		relativePath: string;
+		size: number;
+		isVideo: boolean;
+	}[];
 	await Promise.allSettled(
 		files.map(async (file) => {
 			const relativeFilePath = path.join(relativeDir, file);
 			const absoluteFilePath = path.join(MEDIAS_PATH, relativeFilePath);
 			const stat = await fs.stat(absoluteFilePath);
-
 			if (stat.isDirectory() && !EXCLUDED_FOLDERS.includes(relativeFilePath)) {
-				medias.push(...(await scan(relativeFilePath)));
+				result.push(...(await getCurrentFiles(relativeFilePath)));
 			} else if (stat.isFile() && !EXCLUDED_FILES.includes(relativeFilePath)) {
 				const ext = path.extname(file).toLowerCase();
 				if (!VALID_EXTENSIONS.includes(ext)) return;
-				const isVideo = VIDEO_EXTENSIONS.includes(ext);
-				const fileData = await new Promise<FileData>((resolve, reject) => {
-					ffmpeg.ffprobe(absoluteFilePath, (err, metadata) => {
-						if (err) reject(err);
-						resolve({
-							path: relativeFilePath,
-							size: stat.size,
-							width: metadata.streams[0].width,
-							height: metadata.streams[0].height,
-							duration: isVideo ? metadata.format.duration : 0,
-						});
-					});
+				result.push({
+					absolutePath: absoluteFilePath,
+					relativePath: relativeFilePath,
+					size: stat.size,
+					isVideo: VIDEO_EXTENSIONS.includes(ext),
 				});
-				medias.push(fileData);
 			}
 		}),
 	);
-	return medias;
+	return result;
 }
 
 export default async function scanAndSave() {
-	const newMedias = await scan();
+	const currentFiles = await getCurrentFiles();
 	const res = await prisma.media.findMany({ select: { path: true } });
 	const knownPaths = new Set(res.map((media) => media.path));
-	await prisma.media.createManyAndReturn({
-		data: newMedias.filter((file) => !knownPaths.has(file.path)),
-	});
+	const newMedias = [] as MediaData[];
+	await Promise.allSettled(
+		currentFiles
+			.filter((file) => !knownPaths.has(file.relativePath))
+			.map(async (file) => {
+				newMedias.push(
+					await new Promise<MediaData>((resolve, reject) => {
+						ffmpeg.ffprobe(file.absolutePath, (err, metadata) => {
+							if (err) reject(err);
+							resolve({
+								path: file.relativePath,
+								size: file.size,
+								width: metadata.streams[0].width,
+								height: metadata.streams[0].height,
+								duration: file.isVideo ? metadata.format.duration : 0,
+							});
+						});
+					}),
+				);
+			}),
+	);
+	await prisma.media.createManyAndReturn({ data: newMedias });
 }
